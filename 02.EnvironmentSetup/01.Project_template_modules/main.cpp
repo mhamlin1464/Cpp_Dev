874e36df90826d6536dba423403c4a284a198a63
Mark Hamlin <mhamlin0621@gmail.com>
2025-07-14 17:15:33 -0500

    Add Initial project files

---
+++ 02.EnvironmentSetup/01.Project_template_modules/main.cpp
@@ -0,0 +1,17 @@
+/*
+    . Project description
+        . Topic #1
+        . Topic #2
+*/
+//This will comment out the entire line
+#include <fmt/format.h>
+import utilities;
+
+int main(){
+    int value1 {10};
+    int value2 {20};
+    int result = value1 + value2;
+    fmt::print("Hello Mark \n");
+    fmt::print("Hello World! We are using modern C++ \n");
+    fmt::print("Result: {}\n", result);
+}
